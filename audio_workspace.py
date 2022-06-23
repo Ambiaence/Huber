eb511ad855e260e7e18d20bd159b23b7c2617d96
FocusUser <georgekentallen@gmail.com>
2022-06-23 10:16:05 -0700

    Color is more standardized to config.

--- audio_workspace.py
+++ audio_workspace.py
@@ -2,7 +2,7 @@ import config
 import tkinter as tk
 class AudioWorkspace:
 	def __init__(self):
-		self.audioWindowsFrame = tk.Frame(config.root, bg= "#DDD8B8", height = 855, width = 1920)
+		self.audioWindowsFrame = tk.Frame(config.root, bg= config.colorOne, height = 855, width = 1920)
 		self.audioWindowsFrame.grid(column=0, row=1, sticky = 'e')
 		self.globalButtonsFrame = tk.Frame(config.root, bg= "#B3CBC9", height = 75, width = 1920)
 		self.globalButtonsFrame.grid(column=0, row=2, sticky = 'e')
@@ -46,7 +46,10 @@ class Window:
 		self.visual = Visual(self.frame)
 		self.visual.canvas.bind('<Button-1>', self.pressMouseLeft)
 		self.visual.canvas.bind('<ButtonRelease-1>', self.releaseMouseLeft)
-
+		self.state = "Close-Window"
+		self.openMarker = 0.0
+		self.closeMarker = 1.0
+		self.render()
 
 
 	def fractionToFrame(self, time = 1): #takes a point in time of a song as the fraction of the whole song and returns closest frame to that ideal pint.
@@ -54,13 +57,24 @@ class Window:
 
 	def pressMouseLeft(self, event):
 		print(event.x, event.y, "P:order:", self.order)
+		if self.state == "Close-Window": 
+			self.closeMarker = event.x/config.audioCanvasWidth
+			print(self.closeMarker)
+			self.render()	
 
 	def releaseMouseLeft(self, event):
 		print(event.x, event.y, "R:order:", self.order)
 
+	def render(self):
+		self.visual.canvas.create_rectangle(0,0,config.audioCanvasWidth, config.audioCanvasHeight, fill=config.colorTwo)
+		openPos = int(self.openMarker*config.audioCanvasWidth)
+		self.visual.canvas.create_line(openPos, 0,openPos,config.audioCanvasHeight, fill = config.colorOne)
+		closePos = int(self.closeMarker*config.audioCanvasWidth)
+		self.visual.canvas.create_line(closePos, 0, closePos,config.audioCanvasHeight, fill = config.colorOne)
+
 class Visual:
 	def __init__(self, frame):
 		self.buttonFrame = tk.Frame(frame, height =213, bg = "#B3CBC9", width = 213)
-		self.canvas = tk.Canvas(frame, height = 213, bg = "#DDD8B8", width = "1717")
+		self.canvas = tk.Canvas(frame, height = config.audioCanvasHeight, bg = "#DDD8B8", width = config.audioCanvasWidth)
 		self.canvas.grid(row = 0, column = 1)
 		self.buttonFrame.grid(row = 0, column = 0)
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 root = "nothing" 
 sungio = "test"
+colorOne = "#DDD8B8"
+colorTwo = "#84A9C0"
+audioCanvasWidth = 1717
+audioCanvasHeight = 213
